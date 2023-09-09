f47db41aa89dbf569e1fb685039c2a1c7615fe03
Atharva Dilip Lende <127081975+Atharva1213@users.noreply.github.com>
2023-09-09 13:44:11 +0530

    Delete src/components/core directory

--- src/components/core/CircleImage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import '../assets/css/Home.css';
-export default function CircleImage(props) {
-  return (
-    <div>
-      <img src={props.url} className='circle_img' style={props.style}/>
-    </div>
-  )
-}
--- src/components/core/Map.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Map(props) {
-  return (
-    <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3729.752512060411!2d76.2084641!3d20.801298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd9eec43ea2025d%3A0x3f835853339b7ee3!2sKiran%20Paper%26%20Board%20Industries!5e0!3m2!1sen!2sin!4v1693722493128!5m2!1sen!2sin" width="600" height="450" style={props.style} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-    </div>
-  )
-}
