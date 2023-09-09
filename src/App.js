d30c4d33a39e059d1a90cc2abe4c8b1768867234
Atharva Dilip Lende <127081975+Atharva1213@users.noreply.github.com>
2023-09-09 13:54:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-
 import { Routes,Route } from 'react-router-dom';
 import './App.css';
-import { ContactPage } from './components/core/ContactPage';
-import  { Home } from "./components/core/Home";
+import ContactPage from './Page/ContactPage.js';
+import HomeBanner from './Page/HomeBanner.js';
+import Header from "./Components/Header.js";
+import Product from "./Page/Product_list.js";
+import About from "./Page/About.js";
+import Qualified from "./Page/Qualified.js";
+
 function App() {
   return (
     <div className="App">
-     <Routes>
-       <Route path="/contactpage" element={<ContactPage />} />
-       <Route path="/" element={<Home/>} />
-     </Routes>
+        {/* Header part import */}
+        <Header />
+        {/* Router import kela ahe  */}
+        <Routes>
+        {/* link to contactpage */}
+        <Route path="/Contactpage" element={<ContactPage />} />
+        {/* link to About page */}
+        <Route path="/About" element={<About/>} />
+        {/* link  to Qualified Insurance */}
+        <Route path="/Qualified" element={<Qualified/>} />
+        {/* link to Product page */}
+        <Route path="/Product" element={<Product/>} />
+        {/* link to homepage */}
+        <Route path="/" element={<HomeBanner/>} />
+      </Routes>
     </div>
   );
 }
